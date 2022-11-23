8bbd2abd3d5e0f12eaca912b06cb1b509f99e483
Micheleg27 <michele.garribba27@github.com>
2022-11-23 14:24:29 +0100

    cdd

--- src/pages/Serverstatus.js
+++ src/pages/Serverstatus.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Accordion } from "../Components/ServerStatusComponents/Accordion";
 import { IssueSection } from "../Components/ServerStatusComponents/IssueSection";
 import { NavServer } from "../Components/ServerStatusComponents/NavServer";
 import { TitleServer } from "../Components/ServerStatusComponents/TitleServer";
@@ -10,6 +11,7 @@ export function ServerStatus(){
             <NavServer />
             <TitleServer />
             <IssueSection />
+            <Accordion serverName={'US Central [RTX 3080 Ready]'} serverStatus={'Ready'}/>
         </React.Fragment>
     )
 }
