c51918aa9eb50a22ca9f9c71daea9200d0d22576
Angelo <angelo_derosa95@live.it>
2022-12-13 17:50:43 +0100

    fix: fixed style home

--- Server/server.test.ts
+++ Server/server.test.ts
@@ -7,7 +7,7 @@ const app = createApp();
 const request = supertest(app);
 
 test('POST /login', async () => {
-	const res = await request.post('/register')
+	const res = await request.post('/login')
   .expect(200)
   .expect('Content-Type', 'application/json');
 	expect(res.body).toEqual('http://localhost:3000');
