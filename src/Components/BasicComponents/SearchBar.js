628be04b462cff960479644224d231cc1b3e1288
Angelo <angelo_derosa95@live.it>
2022-12-15 16:48:33 +0100

    fix: fixed searchbar giochi

--- src/Components/BasicComponents/SearchBar.js
+++ src/Components/BasicComponents/SearchBar.js
@@ -1,25 +1,26 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { HiOutlineSearch } from 'react-icons/hi';
+import { IoClose } from 'react-icons/io5';
 
 export function SearchBar() {
-	const [fetch, setFetch] = useState([]);
-	const [search, setSearch] = useState('');
+	const [ fetch, setFetch ] = useState([]);
+	const [ search, setSearch ] = useState('');
 
 	const getFetch = async () => {
 		const response = await axios.get(
 			`https://api.rawg.io/api/games?key=faf20c72c8ba41e1a0a31ddbca2e4de6&search=${search}`
 		);
-		setFetch([...response.data.results]);
+		setFetch([ ...response.data.results ]);
 	};
 	const clearInput = () => {
 		setFetch([]);
-		setSearch("");
+		setSearch('');
 	};
 
 	return (
-		<div className='mb-5'>
-			<div id="div-bar-games">
-						
+		<div className="mb-5">
+			<div id="div-bar-games" style={{ position: 'relative' }}>
 				<input
 					type="search"
 					name="search"
@@ -28,43 +29,30 @@ export function SearchBar() {
 					value={search}
 					onChange={(e) => setSearch(e.currentTarget.value)}
 				/>
-				<button type="button" onClick={getFetch} class="btn submit_1 text-light " data-original-title="" title=""> Cerca </button> 
-				{/* <input type="submit" class="btn" value="Search" onClick={getFetch}></input> */}
-				{/* <div className="searchIcon">
-				{
-					fetch.length === 0 ? (
-						<button style={{backgroundColor:'black'}}onClick={getFetch}>Cerca</button>
+
+				<div className="searchIcon">
+					{fetch.length === 0 ? (
+						<button style={{backgroundColor:'transparent', border:'none'}} onClick={getFetch}>
+							<HiOutlineSearch color="grey" />
+						</button>
 					) : (
-						<button id="clearBtn" onClick={clearInput}>X</button>
-					)
-				}
-				</div> */}
-
-
-				{/* <div className="searchIcon" onClick={getFetch}>
-					<svg
-						id="lente1"
-						xmlns="http://www.w3.org/2000/svg"
-						width="22"
-						height="22"
-						fill="white"
-						class="bi bi-search"
-						viewBox="0 0 16 16"
-					>
-						<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-					</svg>
-				</div> */}
+						<button style={{backgroundColor:'transparent', border:'none'}} onClick={clearInput}>
+							<IoClose color="grey" />
+						</button>
+					)}
+				</div>
 			</div>
-			{fetch.length != 0 && (
-				<div className='dataResult'>
+
+			{fetch.length !== 0 && (
+				<div className="dataResult">
 					{fetch.map((el) => (
-						<div>
-							<h1 className='dataItem'>{el.name}</h1>
+						<div className='d-flex flex-row-reverse'>
+							<h1 className="dataItem">{el.name}</h1>
+              <img src={el.background_image} style={{width:'80px', height:'60px', padding:'5px'}}/>
 						</div>
 					))}
 				</div>
 			)}
-
 		</div>
 	);
 }
